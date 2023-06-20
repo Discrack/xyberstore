bb53f6fb99ab412392f87b79a859c673d12becdd
Discrack <aigalkurniawan@gmail.com>
2023-06-20 08:51:23 +0700

    a

--- profil.php
+++ profil.php
@@ -27,7 +27,7 @@
 				<li><a href="profil.php">Profil</a></li>
 				<li><a href="data-kategori.php">Data Kategori</a></li>
 				<li><a href="data-produk.php">Data Produk</a></li>
-				<li><a href="keluar.php">Keluar</a></li>
+				<li><a href="keluar.php">Logout</a></li>
 			</ul>
 		</div>
 	</header>
